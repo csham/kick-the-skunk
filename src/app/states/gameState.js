04ec78646359445548af938e0dc1a4e7ab54d5a4
Chris Sham <chris.sham@patlive.com>
2016-05-27 16:35:39 -0400

    More tweaks for skunk

--- src/app/states/gameState.js
+++ src/app/states/gameState.js
@@ -25,25 +25,111 @@ define(['constants/stateConstants',
                 constraint.collideConnected = false;
             };
 
-            var buildSkunk = function() {
+            var buildSkunk = function(positionX, positionY) {
                 var skunk = {};
-                skunk.tail = self.game.add.sprite(370, 150, 'skunk-tail');
-                skunk.legLeft = self.game.add.sprite(446, 342, 'skunk-leg-left');
-                skunk.legRight = self.game.add.sprite(416, 342, 'skunk-leg-right');
-                skunk.footLeft = self.game.add.sprite(450, 352, 'skunk-foot-left');
-                skunk.footRight = self.game.add.sprite(400, 352, 'skunk-foot-right');
-                skunk.armLeft = self.game.add.sprite(446, 275, 'skunk-arm-left');
-                skunk.armRight = self.game.add.sprite(400, 275, 'skunk-arm-right');
-                skunk.handLeft = self.game.add.sprite(462, 240, 'skunk-hand-left');
-                skunk.handRight = self.game.add.sprite(384, 240, 'skunk-hand-right');
-                skunk.torso = self.game.add.sprite(400, 240, 'skunk-body');
-                skunk.head = self.game.add.sprite(400, 200, 'skunk-head', 0);
+                //skunk.tail = self.game.add.sprite(positionX - 30, positionY - 90, 'skunk-tail');
+                //self.game.physics.p2.enableBody(skunk.tail);
 
-                self.game.physics.p2.enable([skunk.tail, skunk.legLeft, skunk.legRight, skunk.footLeft, skunk.footRight, skunk.armLeft, skunk.armRight, skunk.handLeft, skunk.handRight, skunk.torso, skunk.head]);
+                skunk.legLeft = self.game.add.sprite(positionX + 46, positionY + 102, 'skunk-leg-left');
+                self.game.physics.p2.enableBody(skunk.legLeft);
 
-                addConstraint(skunk.torso, [0, -40], skunk.head, [0, 0]);
-                addConstraint(skunk.torso, [8, 51], skunk.legLeft, [0, 0]);
-                addConstraint(skunk.torso, [-8, 51], skunk.legRight, [0, 0]);
+                skunk.legRight = self.game.add.sprite(positionX + 16, positionY + 102, 'skunk-leg-right');
+                self.game.physics.p2.enableBody(skunk.legRight);
+
+                skunk.footLeft = self.game.add.sprite(positionX + 50, positionY + 112, 'skunk-foot-left');
+                self.game.physics.p2.enableBody(skunk.footLeft);
+
+                skunk.footRight = self.game.add.sprite(positionX, positionY + 112, 'skunk-foot-right');
+                self.game.physics.p2.enableBody(skunk.footRight);
+
+                skunk.armLeft = self.game.add.sprite(positionX + 46, positionY + 35, 'skunk-arm-left');
+                self.game.physics.p2.enableBody(skunk.armLeft);
+
+                skunk.armRight = self.game.add.sprite(positionX, positionY + 35, 'skunk-arm-right');
+                self.game.physics.p2.enableBody(skunk.armRight);
+
+                skunk.handLeft = self.game.add.sprite(positionX + 62, positionY, 'skunk-hand-left');
+                self.game.physics.p2.enableBody(skunk.handLeft);
+
+                skunk.handRight = self.game.add.sprite(positionX - 16, positionY, 'skunk-hand-right');
+                self.game.physics.p2.enableBody(skunk.handRight);
+
+                skunk.torso = self.game.add.sprite(positionX, positionY, 'skunk-body');
+                self.game.physics.p2.enableBody(skunk.torso);
+
+                skunk.head = self.game.add.sprite(positionX, positionY - 40, 'skunk-head', 0);
+                self.game.physics.p2.enableBody(skunk.head);
+
+                //skunk.tail.revolute = self.game.physics.p2.createLockConstraint(skunk.tail, skunk.torso, [0, -50]);
+
+                skunk.head.revolute = self.game.physics.p2.createRevoluteConstraint(skunk.head, [0, 0], skunk.torso, [-3, -60]);
+                skunk.head.revolute.collideConnected = false;
+                skunk.head.revolute.lowerLimitEnabled = true;
+                skunk.head.revolute.upperLimitedEnabled = true;
+                skunk.head.revolute.lowerLimit = -0.7;
+                skunk.head.revolute.upperLimit = 0.7;
+
+                skunk.armLeft.revolute = self.game.physics.p2.createRevoluteConstraint(skunk.armLeft, [-9, 2], skunk.torso, [13, -4]);
+                skunk.armLeft.revolute.collideConnected = false;
+                skunk.armLeft.revolute.lowerLimitEnabled = true;
+                skunk.armLeft.revolute.upperLimitedEnabled = true;
+                skunk.armLeft.revolute.lowerLimit = -0.7;
+                skunk.armLeft.revolute.upperLimit = 0.7;
+
+                skunk.armRight.revolute = self.game.physics.p2.createRevoluteConstraint(skunk.armRight, [9, 2], skunk.torso, [-13, -4]);
+                skunk.armRight.revolute.collideConnected = false;
+                skunk.armRight.revolute.lowerLimitEnabled = true;
+                skunk.armRight.revolute.upperLimitedEnabled = true;
+                skunk.armRight.revolute.lowerLimit = -0.7;
+                skunk.armRight.revolute.upperLimit = 0.7;
+
+                skunk.legLeft.revolute = self.game.physics.p2.createRevoluteConstraint(skunk.legLeft, [0, -6], skunk.torso, [15, 48]);
+                skunk.legLeft.revolute.collideConnected = false;
+                skunk.legLeft.revolute.lowerLimitEnabled = true;
+                skunk.legLeft.revolute.upperLimitedEnabled = true;
+                skunk.legLeft.revolute.lowerLimit = -0.7;
+                skunk.legLeft.revolute.upperLimit = 0.7;
+
+                skunk.legRight.revolute = self.game.physics.p2.createRevoluteConstraint(skunk.legRight, [0, -6], skunk.torso, [-15, 48]);
+                skunk.legRight.revolute.collideConnected = false;
+                skunk.legRight.revolute.lowerLimitEnabled = true;
+                skunk.legRight.revolute.upperLimitedEnabled = true;
+                skunk.legRight.revolute.lowerLimit = -0.7;
+                skunk.legRight.revolute.upperLimit = 0.7;
+
+                skunk.handLeft.revolute = self.game.physics.p2.createRevoluteConstraint(skunk.handLeft, [-6, 15], skunk.armLeft, [9, -6]);
+                skunk.handLeft.revolute.collideConnected = false;
+                skunk.handLeft.revolute.lowerLimitEnabled = true;
+                skunk.handLeft.revolute.upperLimitedEnabled = true;
+                skunk.handLeft.revolute.lowerLimit = -0.7;
+                skunk.handLeft.revolute.upperLimit = 0.7;
+
+                skunk.handRight.revolute = self.game.physics.p2.createRevoluteConstraint(skunk.handRight, [-6, 15], skunk.armRight, [-9, -6]);
+                skunk.handRight.revolute.collideConnected = false;
+                skunk.handRight.revolute.lowerLimitEnabled = true;
+                skunk.handRight.revolute.upperLimitedEnabled = true;
+                skunk.handRight.revolute.lowerLimit = -0.7;
+                skunk.handRight.revolute.upperLimit = 0.7;
+
+                skunk.footLeft.revolute = self.game.physics.p2.createRevoluteConstraint(skunk.footLeft, [12, -3], skunk.legLeft, [1, -6]);
+                skunk.footLeft.revolute.collideConnected = false;
+                skunk.footLeft.revolute.lowerLimitEnabled = true;
+                skunk.footLeft.revolute.upperLimitedEnabled = true;
+                skunk.footLeft.revolute.lowerLimit = -0.7;
+                skunk.footLeft.revolute.upperLimit = 0.7;
+
+                skunk.footRight.revolute = self.game.physics.p2.createRevoluteConstraint(skunk.footRight, [12, -3], skunk.legRight, [-1, -6]);
+                skunk.footRight.revolute.collideConnected = false;
+                skunk.footRight.revolute.lowerLimitEnabled = true;
+                skunk.footRight.revolute.upperLimitedEnabled = true;
+                skunk.footRight.revolute.lowerLimit = -0.7;
+                skunk.footRight.revolute.upperLimit = 0.7;
+
+                skunk.torso.body.static = true;
+
+                setTimeout(function() {
+                    skunk.torso.body.static = false;
+                }, 2000);
             };
 
             self.preload = function() {
@@ -54,7 +140,12 @@ define(['constants/stateConstants',
                 self.game.physics.startSystem(Phaser.Physics.P2JS);
                 self.game.physics.p2.gravity.y = 750;
 
-                buildSkunk();
+                var rock = self.game.add.sprite(300, 500, 'rock');
+                self.game.physics.p2.enableBody(rock);
+                rock.body.addPolygon({}, [[0, 156], [18, 57], [48, 49], [62, 15], [99, 0], [169, 13], [185, 72], [205.95, 75.7], [227.92, 155.26], [173, 181], [105, 181], [84, 159], [24, 169], [0, 156]]);
+                rock.body.static = true;
+
+                buildSkunk(410, 400);
             };
 
             self.update = function() {
