3dba789211a5cd7c1a2eb6144361693171d8a60c
Chris Sham <chris.sham@patlive.com>
2016-07-29 15:05:28 -0400

    Adding physics data to skunk body parts

--- build/assets/js/app/states/gameState.js
+++ build/assets/js/app/states/gameState.js
@@ -4,7 +4,7 @@ define(['constants/stateConstants',
         return function() {
             var self = this;
 
-            var DEBUG_MODE = false;
+            var DEBUG_MODE = true;
             var WORLD_GRAVITY = 1200;
             var BOOT_MASS = 10;
 
@@ -31,9 +31,15 @@ define(['constants/stateConstants',
                 _mouseBody.position[1] = self.game.physics.p2.pxmi(pointer.position.y);
             };
 
-            var _setupSkunkBodyPhysics = function(skunkBodyPart, skunkCollisionGroup, bootCollisionGroup, rockCollisionGroup) {
+            var _setupSkunkBodyPhysics = function(skunkBodyPart, skunkCollisionGroup, bootCollisionGroup, rockCollisionGroup, physicsDataElement) {
                 skunkBodyPart.body.debug = DEBUG_MODE;
                 skunkBodyPart.body.mass = 1;
+
+                if (physicsDataElement) {
+                    skunkBodyPart.body.clearShapes();
+                    skunkBodyPart.body.loadPolygon('physicsData', physicsDataElement);
+                }
+
                 skunkBodyPart.body.setCollisionGroup(skunkCollisionGroup);
                 skunkBodyPart.body.collides(bootCollisionGroup);
                 skunkBodyPart.body.collides(rockCollisionGroup);
@@ -129,7 +135,7 @@ define(['constants/stateConstants',
                 _mouseBody = new p2.Body();
                 self.game.physics.p2.world.addBody(_mouseBody);
 
-                _setupSkunkBodyPhysics(skunkTail, skunkCollisionGroup, bootCollisionGroup, rockCollisionGroup);
+                _setupSkunkBodyPhysics(skunkTail, skunkCollisionGroup, bootCollisionGroup, rockCollisionGroup, 'skunkTail');
                 _setupSkunkBodyPhysics(skunkLeftArm, skunkCollisionGroup, bootCollisionGroup, rockCollisionGroup);
                 _setupSkunkBodyPhysics(skunkLeftHand, skunkCollisionGroup, bootCollisionGroup, rockCollisionGroup);
                 _setupSkunkBodyPhysics(skunkRightArm, skunkCollisionGroup, bootCollisionGroup, rockCollisionGroup);
@@ -138,8 +144,8 @@ define(['constants/stateConstants',
                 _setupSkunkBodyPhysics(skunkLeftFoot, skunkCollisionGroup, bootCollisionGroup, rockCollisionGroup);
                 _setupSkunkBodyPhysics(skunkRightLeg, skunkCollisionGroup, bootCollisionGroup, rockCollisionGroup);
                 _setupSkunkBodyPhysics(skunkRightFoot, skunkCollisionGroup, bootCollisionGroup, rockCollisionGroup);
-                _setupSkunkBodyPhysics(skunkBody, skunkCollisionGroup, bootCollisionGroup, rockCollisionGroup);
-                _setupSkunkBodyPhysics(skunkHead, skunkCollisionGroup, bootCollisionGroup, rockCollisionGroup);
+                _setupSkunkBodyPhysics(skunkBody, skunkCollisionGroup, bootCollisionGroup, rockCollisionGroup, 'skunkBody');
+                _setupSkunkBodyPhysics(skunkHead, skunkCollisionGroup, bootCollisionGroup, rockCollisionGroup, 'skunkHead');
 
                 _setupSkunkBodyRotationalConstraint({bodyA: skunkBody, bodyB: skunkLeftArm, bodyAPivotPointOffset: [14, -4], bodyBPivotPointOffset: [-11, 4], minRotationDegrees: -50, maxRotationDegrees: 90});
                 _setupSkunkBodyRotationalConstraint({bodyA: skunkBody, bodyB: skunkRightArm, bodyAPivotPointOffset: [-14, -4], bodyBPivotPointOffset: [11, 4], minRotationDegrees: -90, maxRotationDegrees: 50});
